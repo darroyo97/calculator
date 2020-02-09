321699949cea2b6b0d5231b7613e2941966fb7b2
Daniela <daniela.arroyo27@gmail.com>
2020-02-09 17:03:10 -0600

    fixed error with equal sign

--- js/script.js
+++ js/script.js
@@ -9,8 +9,31 @@ let stringStorage = [];
 
 let sum = 0;
 
+let validChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
+
+let validOpChar = ['+']
+
+buttons.addEventListener('click', function (e) {
+    // console.log(e.target)
+    // e.target is not an equal sign or a clear sign then append a str to
+    // the input
+    // when is equal sign we going to split the string by the plus symbol
+    //add all content and remove the reduce method
+    //display result on our screen
+    //if c reset all of our arrays and values
+
+    if (validChar.includes(e.target.innerText)) {
+        // console.log('found');
+        input.textContent = input.textContent + e.target.textContent
+    }
+    else if (validOpChar.includes(e.target.textContent)) {
+        input.textContent = input.textContent + e.target.textContent
+    }
+    else if (e.target.textContent == "=") {
+        console.log(input.textContent)
+    }
 
 
-
+})
 
 
